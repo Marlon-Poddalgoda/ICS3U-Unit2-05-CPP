3c4310b41473325750912a4ec26457718bb3a8f8
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2020-11-26 17:15:43 +0000

    added local_and_global.cpp

---
+++ local_and_global.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2020 Marlon Poddalgoda, All rights reserved
+//
+// Created by Marlon Poddalgoda
+// Created on November 2020
+// This program shows how local and global variables work
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+
+void globalVariable() {
+    // this shows what happens with global variables
+
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Global variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+int main() {
+    // this function calls local and global
+
+    localVariable();
+    globalVariable();
+}
